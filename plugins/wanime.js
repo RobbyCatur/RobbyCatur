029d31210c7095f6cdf556d0b8c7a622920f64c8
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-08 20:49:09 +0700

    Update wanime.js

--- plugins/wanime.js
+++ plugins/wanime.js
@@ -1,4 +1,5 @@
 let handler = async (m, { conn }) => {
+  m.reply('Proses')
   let me = conn.user.name
   conn.sendFile(m.chat, (`https://api.lolhuman.xyz/api/random/wallnime?apikey=c6670fc7e461b7623a8fdf9f`), '', `Wallpaper for you by ${me}`, m)
   }
