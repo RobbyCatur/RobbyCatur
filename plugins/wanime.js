04c6b34ddf055498dd90e1a2905906dbc9851e72
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-08 15:40:46 +0700

    Update wanime.js

--- plugins/wanime.js
+++ plugins/wanime.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
   let me = conn.user.name
-  conn.sendFile(m.chat, (`https://api.lolhuman.xyz/api/random/wallnime?apikey=c6670fc7e461b7623a8fdf9f`), `Wallpaper for you by ${me}`, m)
+  conn.sendFile(m.chat, (`https://api.lolhuman.xyz/api/random/wallnime?apikey=c6670fc7e461b7623a8fdf9f`), '', `Wallpaper for you by ${me}`, m)
   }
   handler.command = /^wanime$/i
   module.exports = handler
