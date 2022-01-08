e4ff3e92bfab88600087023e2855617f60f537cb
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-08 08:27:52 +0700

    Update igmp3.js

--- plugins/igmp3.js
+++ plugins/igmp3.js
@@ -5,5 +5,6 @@ let handler = async (m, { conn, args }) => {
   let json = await res.json()
   let result = json.url_mp3
   conn.sendFile(m.chat, result, 'ig.mp3', m)
+}
   handler.command = /^igmp3$/i
   module.exports = handler
