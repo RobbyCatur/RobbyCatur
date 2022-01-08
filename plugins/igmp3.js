a0fdc5211bf96e96cd95595f90480710a9687b06
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-08 08:31:26 +0700

    Add files via upload

---
+++ plugins/igmp3.js
@@ -0,0 +1,9 @@
+const fetch = require('node-fetch')
+
+let handler = async (m, { conn, args }) => {
+  let res = await fetch(`https://hardianto.xyz/api/instagram?url=` + args[0])
+  let json = await res.json()
+  conn.sendFile(m.chat, ${json.url_mp3}, m)
+  }
+  handler.command = /^igmp3$/i
+  module.exports = handler
