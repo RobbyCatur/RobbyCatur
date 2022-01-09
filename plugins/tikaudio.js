fc7081d7321ffb696d082e88c4fb898bc428683b
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-09 11:55:34 +0700

    Update tikaudio.js

--- plugins/tikaudio.js
+++ plugins/tikaudio.js
@@ -2,5 +2,5 @@ let handler = async (m, { conn, args }) => {
   m.reply('Proses')
   conn.sendFile(m.chat, (`https://api.lolhuman.xyz/api/tiktokmusic?apikey=c6670fc7e461b7623a8fdf9f&url=` + args[0]), '', '', m)
   } 
-  handler.command = /^t(ik?)t(ok?)(audio|mp3)$/i
+  handler.command = /^tik(tok)?(audio|mp3)|tt(audio|mp3)$/i
   module.exports = handler
