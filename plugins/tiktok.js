532a640abf288780b9e5d2ceb21e52f67f993568
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-09 07:22:11 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, args }) => {
 	m.reply('Proses')
   if (!args[0]) throw 'Uhm...url nya mana?'
   let me = conn.user.name
-  let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=c6670fc7e461b7623a8fdf9f&url=` + args[0])
+  let res = await fetch(`https://api.lolhuman.xyz/api/tiktok3?apikey=c6670fc7e461b7623a8fdf9f&url=` + args[0])
   if (res.status !== 200) throw `Server error!`
   let json = await res.json()
 //  if (!json.status) throw json
