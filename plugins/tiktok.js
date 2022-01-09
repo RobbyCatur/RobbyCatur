b0428a39f6095e14bfb04591a244753cec982174
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-09 07:14:31 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -16,6 +16,6 @@ ${me} Tiktok Downloader
 handler.help = ['tiktok', 'tt', 'tik'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
-handler.command = /^(tt|tik(tok)?(dl)?)$/i
+handler.command = /^t(ik)?t(ok)?dl$/i
 handler.limit = true
 module.exports = handler
