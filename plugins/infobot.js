d1c21d98b261bd6eac3151944276b8cfc395a6f6
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-11 10:56:35 +0700

    Update infobot.js

--- plugins/infobot.js
+++ plugins/infobot.js
@@ -5,17 +5,6 @@ let handler = async (m, { conn, _p }) => {
   let user = global.db.data.users
   let totalreg = Object.keys(global.db.data.users).length
   let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
-  let d = new Date(new Date + 3600000)
-  let locale = 'id'
-    // d.getTimeZoneOffset()
-    // Offset -420 is 18.00
-    // Offset    0 is  0.00
-    // Offset  420 is  7.00
-  let time = d.toLocaleTimeString(locale, {
-      hour: 'numeric',
-      minute: 'numeric',
-      second: 'numeric'
-    })
   let _uptime = process.uptime() * 1000
   let uptime = clockString(_uptime)
   let noww = time - uptime
@@ -34,7 +23,12 @@ Bot telah berjalan selama ${uptime}
 Bot telah berjalan pada ${noww}
 `.trim(), m)
 }
+handler.help = ['infobot']
+handler.tags = ['owner']
 handler.command = /^infobot$/i
+
+handler.owner = true
+
 module.exports = handler
 
 function clockString(ms) {
