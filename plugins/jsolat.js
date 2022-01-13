6230ba680ba6b00eeef686a25fa47575590765f2
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-14 06:47:19 +0700

    Add files via upload

--- plugins/jsolat.js
+++ plugins/jsolat.js
@@ -1,6 +1,7 @@
 const fetch = require('node-fetch')
 
 let handler = async (m, { conn, text } => {
+	m.reply('Proses')
   let res = await fetch(`https://docs-jojo.herokuapp.com/api/jadwalshalat?daerah=` + text )
   let json = await res.json()
   let result = `
