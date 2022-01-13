a70c4506e52ec6d1bdedeb5a186189869235a809
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-14 06:14:50 +0700

    Add files via upload

---
+++ plugins/jsolat.js
@@ -0,0 +1,21 @@
+const fetch = require('node-fetch')
+
+let handler = async (m, { conn, text } => {
+  let res = await fetch(`https://docs-jojo.herokuapp.com/api/jadwalshalat?daerah=` + text )
+  let json = await res.json()
+  let result = `
+  Jadwal sholat untuk ${text}
+
+Imsyak : ${json.Imsyak}
+Subuh : ${json.Subuh}
+Dhuha : ${json.Dhuha}
+Dzuhur : ${json.Dzuhur}
+Ashar : ${json.Ashar}
+Maghrib : ${json.Maghrib}
+Isya : ${json.Isya}
+`.trim()
+  if (result) await m.reply(result)
+  else throw `Kota tidak ditemukan`
+  }
+  handler.conmand = /^j(adwal)?(sholat|salat|solat|shalat)$/i
+  module.exports = handler
