f78dbb4e9d99bdeb5b3244699117cf5a5ee4bbca
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-13 18:39:37 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,18 +1,17 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, args, isOwner }) => {
 	m.reply('Proses')
-  if (!args[0]) throw 'Uhm...url nya mana?'
+  if (!args[0]) throw 'Send linknya'
  // if (!isOwner) throw `Maaf, sementara fitur ini dinonaktifkan dulu karena terdapat bug`
   let me = conn.user.name
-  let res = await fetch(`https://api.lolhuman.xyz/api/tiktok?apikey=c6670fc7e461b7623a8fdf9f&url=` + args[0])
-  if (res.status !== 200) throw `Server error!`
+  let res = await fetch(`https://docs-jojo.herokuapp.com/api/tiktok_wm?url=` + args[0])
   let json = await res.json()
-//  if (!json.status) throw json
-  let url = json.result.link
+  let url = json.download
   let txt = `
 ${me} Tiktok Downloader
     `
-    await conn.sendFile(m.chat, url, 'tiktok.mp4', txt.trim(), m)
+    if (json.download) await conn.sendFile(m.chat, url, 'tiktok.mp4', txt.trim(), m)
+      else throw `Server error`
 }
 handler.help = ['tiktok', 'tt', 'tik'].map(v => v + ' <url>')
 handler.tags = ['downloader']
