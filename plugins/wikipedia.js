5f097797983aff270183c5f872e6dcd3da617008
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-14 08:32:09 +0700

    Add files via upload

---
+++ plugins/wikipedia.js
@@ -0,0 +1,11 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn }) => {
+  let res = await fetch('https://docs-jojo.herokuapp.com/api/wiki?q=' + text )
+  let json = await res.json()
+  let result = json.result
+  let img = json.img[0]
+  conn.sendFile(m.chat, img, 'img.jpg', result, m)
+  }
+  handler.command = /^wiki|wikipedia$/i
+  module.exports = handler
