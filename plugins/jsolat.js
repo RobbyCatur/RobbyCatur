4fc13998ab8405c52f1d23fbbac027be60fb3df3
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-14 07:11:29 +0700

    Update jsolat.js

--- plugins/jsolat.js
+++ plugins/jsolat.js
@@ -1,9 +1,10 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text }) => {
-let res = await fetch(`https://docs-jojo.herokuapp.com/api/jadwalshalat?daerah=` + text )
-let json = await res.json()
-let result = `
+  m.reply('Proses')
+  let res = await fetch(`https://docs-jojo.herokuapp.com/api/jadwalshalat?daerah=` + text )
+  let json = await res.json()
+  let result = `
 Jadwal sholat untuk ${text}
 
 Imsyak : ${json.Imsyak}
@@ -14,7 +15,7 @@ Ashar : ${json.Ashar}
 Maghrib : ${json.Maghrib}
 Isya : ${json.Isya}
 `.trim()
-m.reply(result)
+  m.reply(result)
   }
   handler.command = /^jsolat$/i
   module.exports = handler
