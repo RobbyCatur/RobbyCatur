2790e9d4b7c6792122f72f02a30f5d5015357fbc
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-14 23:48:49 +0700

    Update fb.js

--- plugins/fb.js
+++ plugins/fb.js
@@ -2,11 +2,11 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, args }) => {
 	m.reply('Proses')
-	let res = await fetch(`https://api.lolhuman.xyz/api/facebook?apikey=c6670fc7e461b7623a8fdf9f&url=` + args[0])
-	if (res.status !== 200) throw `Coba Lagi`
+	let res = await fetch(`https://masgimenz.my.id/facebook/?url=https://www.facebook.com/groups/435836803952410/permalink/1019003362302415/?app=fbl` + args[0])
+	//if (res.status !== 200) throw `Coba Lagi`
 	let json = await res.json()
-	if (!json.result) throw `Media tidak ditemukan atau postingan mungkin diprivate`
-	let url = json.result.hd.url || ''
+	//if (!json.result) throw `Media tidak ditemukan atau postingan mungkin diprivate`
+	let url = json.videoUrl
 	if (url) await conn.sendFile(m.chat, url, 'fb.mp4', `Pudge Invoker Facebook Downloader`, m)
 	else m.reply('Link download tidak ditemukan')
 	}
