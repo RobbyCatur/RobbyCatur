526676f2579c0e46eb7569d3bf930dc80681068f
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-01-17 08:01:23 +0700

    Update berita.js

--- plugins/berita.js
+++ plugins/berita.js
@@ -6,7 +6,7 @@ let handler = async (m, { conn }) => {
 	  
 	  axios.get(`https://docs-jojo.herokuapp.com/api/news`).then((res) => {
 		let result = res.data.articles.map(res => `*Judul*: ${res.title}\n*Author*: ${res.author}\n*Publish*: ${res.publishedAt}\n*Spoiler*: ${res.description}\n*URL*: ${res.url}`).join('\n\n================================\n   *PEMBATAS* *PEMBATAS* *PEMBATAS*\n================================\n\n').trim()
-		let pala = `*BREAKING NEWS*`
+		let pala = `*BREAKING NEWS*\n\n`
 		m.reply(pala+result)
 		})
 		}
