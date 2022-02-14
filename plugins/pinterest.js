dc76a91aba11d392ac572cd151c25ce76744c708
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-02-14 23:17:52 +0700

    Update pinterest.js

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn. args }) => {
   let json = await res.json()
   let result = json.result
   let type = json.type
-  conn.sendFile(m.chat, result, 'pinterest' + ${type == 'video' ? '.mp4' : '.jpg', `tes`, m)
+  conn.sendFile(m.chat, result, 'pinterest' + ${type == 'video' ? '.mp4' : '.jpg'}, `tes`, m)
   }
   handler.command = /^pin$/i
 
