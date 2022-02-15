0a1ce2e3415ce32349c5c5065c3277c45f8a1c61
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-02-15 17:10:23 +0700

    Add files via upload

--- plugins/pinterest.js
+++ plugins/pinterest.js
@@ -1,12 +1,14 @@
 let fetch = require('node-fetch')
 
-let handler = async (m, { conn. args }) => {
-  let res = await fetch(`https://docs-jojo.herokuapp.com/api/pinterest?url=` + args[0])
-  let json = await res.json()
-  let result = json.result
-  let type = json.type
-  conn.sendFile(m.chat, result, 'pin' + (type == 'video' ? '.mp4' : '.jpg'), `tes`, m)
-  }
-  handler.command = /^pin$/i
+let handler = async (m, { conn, arg }) => {
+	m.reply('Proses')
+	let me = conn.user.name
+	let res = await fetch(`https://docs-jojo.herokuapp.com/api/pinterest?url=` + args[0])
+	let json = await res.json()
+	let url = json.result
+	let type = json.type
+	conn.sendFile(m.chat, url, 'pin' + ${type == 'video' ? '.mp4' : '.jpg'}, `${me} pinterest downloader`, m)
+	}
+	handler.command = /^pin$/i
 
 module.exports = handler
