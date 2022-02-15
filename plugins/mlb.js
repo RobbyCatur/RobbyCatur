c5cb28604265315afa1a3cc1d73c604d6aa6510d
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-02-16 06:28:49 +0700

    Update mlb.js

--- plugins/mlb.js
+++ plugins/mlb.js
@@ -4,22 +4,23 @@ let handler = async (m, { conn }) => {
 *_Via ID - Legal 100%_*
 🔰 *_FrihetStore_* 🔰
 
-39 💎 = Rp 8.495
-65 💎 = Rp 14.155
-92 💎 = Rp 19.785
-133 💎 = Rp 28.315
-266 💎 = Rp 56.615
-400 💎 = Rp 84.925
-534 💎 = Rp 113.235
-670 💎 = Rp 141.545
-1342 💎 = Rp 283.135
-2700 💎 = Rp 577.555
-4150 💎 = Rp 866.555
-7050 💎 = Rp 1.500.000
+34+5 = 39💎 Rp 9.000
+58+7 = 65💎 Rp 14.740
+83+9 = 92💎 Rp 20.636
+120+13 = 133💎 Rp 29.480
+240+26 = 266💎 Rp 58.993
+360+40 = 400💎 Rp 88.406
+480+54 = 534💎 Rp 117.920
+600+70 = 670💎 Rp 147.500
+1200+142 = 1342💎 Rp 295.000
+2400+300 = 2700💎 Rp 593.000
+3600+550 = 4150💎 Rp 887.000
+6000+1050 = 7050💎 Rp 1.480.000
 
-
-⏳ *_PROSES 1-30 MENIT_*
+⏳ *PROSES 3-60 MENIT MAX* 
+*24 JAM*
 ⚠️ *NOTE : TANYAKAN STOK SEBELUM MELAKUKAN TRANSFER*
+⚠️ *NOTE : TANDA (+) ATAU BONUS TIDAK TERHITUNG PADA EVENT TOPUP*
 `.trim(), '© Robby Catur', 'BAYAR', '.payment', m)
 }
 handler.command = /^mlb$/i
