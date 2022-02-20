860e552bb741f19c9c630de4d09c0986cfa2f680
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-02-20 08:06:37 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,17 +1,20 @@
-const HX = require('hxz-api')
+let fetch = require('node-fetch')
 
 let handler = async (m, { conn, args }) => {
   m.reply('Proses')
   let me = conn.user.name
+  let res = await fetch('https://api-xcoders.xyz/api/download/ig?url=' + args[0] + '&apikey=DPZDDDpHHM')
+  let json = await res.json()
+  let result = json.result.url
   let txt = `
 ${me} Instagram downloader
 `.trim()
-  let result = HX.igdl(args[0]).then(res => conn.sendFile(m.chat, res.medias[0].url, '', txt, m))
+  conn.sendFile(m.chat, result, 'ig.jpg', txt, m)
 }
 handler.help = ['ig']
 handler.tags = ['downloader']
 handler.command = /^ig$/i
 
-handler.limit = true
+handler.limit = false
 
 module.exports = handler
