7f57d16807f3f53f210002d5346ed658748fe549
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-02-27 20:34:18 +0700

    Create igstalk.js

---
+++ plugins/igstalk.js
@@ -0,0 +1,24 @@
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, text }) => {
+  let res = await fetch('https://api-xcoders.xyz/api/stalk/ig?username=' + text '&apikey=DPZDDDpHHM')
+  let json = await res.json()
+  let result = json.result
+  let pp = result.profile_url
+  let txt = `
+Username: ${result.username}
+Name: ${result.full_name}
+Followers: ${result.followers}
+Following: ${result.following}
+Post : ${result.posts_count}
+Bio: ${result.bio}
+External Url: ${result.external_url}
+Private: ${result.is_private}
+Verified: ${result.is_verified}
+`.trim()
+conn.sendFile(m.chat, pp, 'ppig.jpg', txt, m)
+}
+handler.help = ['igstalk']
+handler.tags = ['stalker']
+handler.command = /^(igstalk)$/i
+module.exports = handler
