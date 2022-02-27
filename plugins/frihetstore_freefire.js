eafedb89efaead39de0c3e68d70bf85be61f5c17
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-02-27 07:53:10 +0700

    Update and rename ff.js to frihetstore_freefire.js

--- plugins/frihetstore_freefire.js
+++ plugins/frihetstore_freefire.js
@@ -4,22 +4,29 @@ let handler = async (m, { conn }) => {
 *_Via ID - Legal 100%_*
 🔰 *FrihetStore* 🔰
     
-5 💎 = Rp 820
-20 💎 = Rp 2.735
-50 💎 = Rp 6.565
-70 💎 = Rp 9.030
-100 💎 = Rp 13.405
-140 💎 = Rp 18.060
+5 💎 = Rp 850
+20 💎 = Rp 2.750
+50 💎 = Rp 6.700
+70 💎 = Rp 9.000
+100 💎 = Rp 13.400
+140 💎 = Rp 18.000
 210 💎 = Rp 27.100
 355 💎 = Rp 45.130
 720 💎 = Rp 90.255
-2000 💎 = Rp 248.000
+ 2000 💎 = Rp 248.000
 7290 💎 = Rp 905.000
 🗿MM= Rp 27.500
 🦏MB = Rp 137.000
 
+*FORMAT ORDER :*
+User id :
+Nickname :
+Nominal Order :
+Sertakan Bukti pembayaran
+
 
 ⏳ *_PROSES 1-10 MENIT_*
+⚠️ *NOTE : TANYAKAN STOK SEBELUM MELAKUKAN TRANSFER*
 `.trim(), '© Robby Catur', 'BAYAR', '.payment', m)
 }
 handler.command = /^ff$/i
