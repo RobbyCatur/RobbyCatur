dbd7e6cc0ccf8f2cc88bb55743cb976d25636e74
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-02-27 07:49:51 +0700

    Update frihetstore_mla.js

--- plugins/frihetstore_mla.js
+++ plugins/frihetstore_mla.js
@@ -1,26 +1,34 @@
 let handler = async (m, { conn }) => {
   conn.sendButton(m.chat, `
-*_PriceList Diamond ML B_*
+*_PriceList Diamond ML A_*
 *_Via ID - Legal 100%_*
-🔰 *_FrihetStore_* 🔰
+🔰 *FrihetStore* 🔰
 
-34+5 = 39💎 Rp 9.222
-58+7 = 65💎 Rp 15.111
-83+9 = 92💎 Rp 20.944
-120+13 = 133💎 Rp 29.920
-240+26 = 266💎 Rp 59.874
-360+40 = 400💎 Rp 89.726
-480+54 = 534💎 Rp 119.680
-600+70 = 670💎 Rp 149.600
-1200+142 = 1342💎 Rp 299.200
-2400+300 = 2700💎 Rp 598.500
-3600+550 = 4150💎 Rp 898.000
-6000+1050 = 7050💎 Rp 1.500.000
+86 💎 = Rp 20.500
+172 💎 =Rp 41.000 
+257 💎 = Rp 61.500 
+344 💎 = Rp 82.000
+429 💎 = Rp 102.500
+514 💎 = Rp 123.000
+600 💎 = Rp 144.000
+706 💎 = Rp 165.000 
+878 💎 = Rp 205.000 
+963 💎 = Rp 225.500 
+1050 💎 = Rp 246.000 
+1220 💎 = Rp 287.000 
+1412 💎 = Rp 328.000 
+2195 💎 =Rp 488.000
+3073 💎 = Rp 693.000
+3688 💎 = Rp 812.000
+4032 💎 = Rp 894.000 
+5532 💎 = Rp 1.219.000 
+6238 💎 = Rp 1.383.000 
+9288 💎 = Rp 2.030.000 
+⭐SL/TW	 Rp95.000
+🌟SL PLUS	 Rp285.000
 
-⏳ *PROSES 3-60 MENIT MAX* 
-*24 JAM*
+⏳ *_PROSES 1-10 MENIT_*
 ⚠️ *NOTE : TANYAKAN STOK SEBELUM MELAKUKAN TRANSFER*
-⚠️ *NOTE : TANDA (+) ATAU BONUS TIDAK TERHITUNG PADA EVENT TOPUP*
 `.trim(), '© Robby Catur', 'BAYAR', '.payment', m)
 }
 
