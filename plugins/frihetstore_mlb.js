b20f91f2c29eb40a0ed30cbc2887470382629b7c
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-02-27 19:49:40 +0700

    Update frihetstore_mlb.js

--- plugins/frihetstore_mlb.js
+++ plugins/frihetstore_mlb.js
@@ -4,23 +4,22 @@ let handler = async (m, { conn }) => {
 *_Via ID - Legal 100%_*
 🔰 *_FrihetStore_* 🔰
 
-34+5 = 39💎 Rp 9.222
-58+7 = 65💎 Rp 15.111
-83+9 = 92💎 Rp 20.944
-120+13 = 133💎 Rp 29.920
-240+26 = 266💎 Rp 59.874
-360+40 = 400💎 Rp 89.726
-480+54 = 534💎 Rp 119.680
-600+70 = 670💎 Rp 149.600
-1200+142 = 1342💎 Rp 299.200
-2400+300 = 2700💎 Rp 598.500
-3600+550 = 4150💎 Rp 898.000
-6000+1050 = 7050💎 Rp 1.500.000
+39     💎 = Rp 9.233
+65     💎 = Rp 15.266
+92     💎 = Rp 21.422
+133   💎 = Rp 30.588
+266   💎 = Rp 61.122
+400   💎 = Rp 91.533
+534   💎 = Rp 122.622
+670   💎 = Rp 152.599
+1342 💎 = Rp 305.122
+2700 💎 = Rp 610.500
+4150 💎 = Rp 915.500
+7050 💎 = Rp 1.525.500
 
 ⏳ *PROSES 3-60 MENIT MAX* 
 *24 JAM*
 ⚠️ *NOTE : TANYAKAN STOK SEBELUM MELAKUKAN TRANSFER*
-⚠️ *NOTE : TANDA (+) ATAU BONUS TIDAK TERHITUNG PADA EVENT TOPUP*
 `.trim(), '© Robby Catur', 'BAYAR', '.payment', m)
 }
 handler.command = /^mlb$/i
