ac995c7fed0832aaf58ee0f4804d7ac38391791b
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-02-27 21:01:49 +0700

    Update igstalk.js

--- plugins/igstalk.js
+++ plugins/igstalk.js
@@ -2,7 +2,7 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, args }) => {
   m.reply('Proses')
-  let res = await fetch('https://api-xcoders.xyz/api/stalk/ig?username=' + args[0] '&apikey=DPZDDDpHHM')
+  let res = await fetch(`https://api-xcoders.xyz/api/stalk/ig?username=` + args[0] `&apikey=DPZDDDpHHM`)
   let json = await res.json()
   let result = json.result
   let pp = result.profile_url
