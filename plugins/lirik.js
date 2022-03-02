87b4dd11baf6ee0cd9943646e69634ab90524758
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-03-02 20:30:14 +0700

    Update lirik.js

--- plugins/lirik.js
+++ plugins/lirik.js
@@ -1,10 +1,15 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text, usedPrefix }) => {
-  if (!text) throw `Masukkan lirik lagu yang ingin dicari!\nContoh: ${usedPrefix}lirik menepi`
+  if (!text) throw 'Masukkan lirik lagu yang ingin dicari!\nContoh: ${usedPrefix}lirik menepi'
   let res = await fetch('https://leyscoders-api.herokuapp.com/api/lirik?q=' + text + '&apikey=dappakntlll')
   let json = await res.json()
-  conn.reply(m.chat, `Lirik lagu ${text}\n\n${json.result}', m)
+  let txt = `
+*Lirik lagu ${text}*
+
+${json.result}
+`.trim()
+  conn.reply(m.chat, txt, m)
 }
 handler.command = /^(lirik)$/i
 module.exports = handler
