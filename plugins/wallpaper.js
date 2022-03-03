585d6e44a5b7d12ef3ac7b709bec952a3505b140
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-03-03 13:03:01 +0700

    Update wallpaper.js

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -6,6 +6,7 @@ let handler = async (m, { conn, text }) => {
   let res = await fetch('https://api.lolhuman.xyz/api/wallpaper?apikey=c6670fc7e461b7623a8fdf9f&query=' + text )
   let json = await res.json()
   let result = json.result
+  if (json.result == 'error') throw `Wallpaper ${text} tidak ditemukan!`
   conn.sendFile(m.chat, result, 'wp.jpg', `Wallpaper ${text}`, m)
   }
   handler.help = ['wp', 'wallpaper'].map(v => v + '<teks>')
