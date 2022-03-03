049e37d931db1aecfacc4da354cc658fc96cf9c2
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-03-03 13:04:21 +0700

    Update wallpaper.js

--- plugins/wallpaper.js
+++ plugins/wallpaper.js
@@ -2,11 +2,11 @@ let fetch = require('node-fetch')
 
 let handler = async (m, { conn, text }) => {
   if (text) m.reply('Proses')
-  else m.reply('Masukkan query!')
+  else m.reply('Ketikkan wallpaper yang ingin dicari!')
   let res = await fetch('https://api.lolhuman.xyz/api/wallpaper?apikey=c6670fc7e461b7623a8fdf9f&query=' + text )
   let json = await res.json()
   let result = json.result
-  if (json.result == 'error') throw `Wallpaper ${text} tidak ditemukan!`
+  if (json.result == error) throw `Wallpaper ${text} tidak ditemukan!`
   conn.sendFile(m.chat, result, 'wp.jpg', `Wallpaper ${text}`, m)
   }
   handler.help = ['wp', 'wallpaper'].map(v => v + '<teks>')
