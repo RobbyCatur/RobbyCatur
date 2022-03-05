39ce87a2f5723dd413fc4b5c578a291b611d891d
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-03-05 08:20:19 +0700

    Update lihat.js

--- plugins/lihat.js
+++ plugins/lihat.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn }) => {
   let q = m.quoted ? m.quoted : m
         if (q.mtype == 'viewOnceMessage') {
-            await conn.copyNForward(m.chat, await this.loadMessage(m.chat, q.id), false, { readViewOnce: true })
+            await conn.copyNForward(m.chat, await conn.loadMessage(m.chat, q.id), false, { readViewOnce: true })
         }
     }
   handler.command = /^(lihat)$/i
