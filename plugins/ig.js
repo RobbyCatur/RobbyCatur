1e8f77ef7097020f1bbeb2c0e5dc8854b0e79a90
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-03-05 11:19:56 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -1,11 +1,11 @@
 let fetch = require('node-fetch')
 
 let handler = async (m, { conn, args }) => {
-  m.reply('Proses')
+  if (args) m.reply('Proses')
+     else m.reply('Masukkan link Instagram yang ingin di download!')
   let me = conn.user.name
-  let res = await fetch('https://api.lolhuman.xyz/api/instagram?apikey=c6670fc7e461b7623a8fdf9f&url=' + args[0])
+  let res = await fetch('https://hadi-api.herokuapp.com/api/instagram?url=' + args[0])
   let json = await res.json()
-  let result = json.result[0]
   let txt = `
 ${me} Instagram downloader
 `.trim()
