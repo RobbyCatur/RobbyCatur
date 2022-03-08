0cbac94f933c24d04c693f9420b09770b7ab58a2
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-03-08 20:23:40 +0700

    Update tiktok.js

--- plugins/tiktok.js
+++ plugins/tiktok.js
@@ -1,18 +1,20 @@
-let fetch = require('node-fetch')
+const ds = require('dandi-api')
 let handler = async (m, { conn, args, isOwner }) => {
-	if (!args[0]) m.reply('Link tiktoknya mana')
+	if (!args[0]) throw `Link tiktoknya mana`
         else m.reply('Proses')
  // if (!isOwner) throw `Maaf, sementara fitur ini dinonaktifkan dulu karena terdapat bug`
-  let me = conn.user.name
-  let res = await fetch(`https://erdwpe-api.herokuapp.com/downloader/tiktok?url=` + args[0])
+  ds.Tiktok(args[0]).then(r => {
+    let me = conn.user.name
+    conn.sendFile(m.chat, r.no_wm, '', `${me} Tiktok Downloader`, m)
+     })
 //  if (res.status !== 200) throw `Server error!`
-  let json = await res.json()
+//  let json = await res.json()
 //  if (!json.status) throw json
-  let url = json.result.nowatermark
-  let txt = `
-${me} Tiktok Downloader
-    `
-    await conn.sendFile(m.chat, url, 'tiktok.mp4', txt.trim(), m)
+//  let url = json.result.nowatermark
+//  let txt = `
+//  ${me} Tiktok Downloader
+//   `
+//   await conn.sendFile(m.chat, url, 'tiktok.mp4', txt.trim(), m)
 }
 handler.help = ['tiktok', 'tt', 'tik'].map(v => v + ' <url>')
 handler.tags = ['downloader']
