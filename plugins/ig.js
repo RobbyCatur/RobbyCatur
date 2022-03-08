081589eb4fa54cfb280ec79693b509cbf350aeca
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-03-08 12:40:09 +0700

    Update ig.js

--- plugins/ig.js
+++ plugins/ig.js
@@ -3,13 +3,13 @@ const hx = require('hxz-api')
 let handler = async (m, { conn, args }) => {
   if (!(args[0])) throw `Masukkan link Instagram yang ingin didownload!`
      else m.reply('Proses')
-  hx.igdl(args[0]).then(r => {
+  hx.igdl(args[0]).then(async (r) => {
   let me = conn.user.name
   let txt = `
 ${me} Instagram downloader
 `.trim()
   for (let i = 0; i < r.medias.length; i++) {
-  conn.sendFile(m.chat, r.medias[i].url, '', txt, m)
+    conn.sendFile(m.chat, r.medias[i].url, '', txt, m)
     }
   })
 }
