a6c6372ab9217c815f70f1031a581345fdf4d187
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-03-21 20:06:12 +0700

    Update asupan.js

--- plugins/asupan.js
+++ plugins/asupan.js
@@ -1,8 +1,11 @@
 let fetch = require('node-fetch')
+const moment = require('moment-timezone')
 
 let handler = async (m, { conn }) => {
   m.reply('Proses')
   let me = conn.user.name
+  const time = moment.tz('Asia/Jakarta').format('HH')
+  if (time >= 18) throw `Udah malem bang, waktunya tidur bukan ngocok`
   let res = await fetch(`https://api.lolhuman.xyz/api/asupan?apikey=c6670fc7e461b7623a8fdf9f`)
   let json = await res.json()
   let vid = json.result
