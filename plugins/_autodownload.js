9eac6e221b1ad76ab3290bf78c04c9cfeff62d30
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-03-22 08:24:25 +0000

    Add files via upload

--- plugins/_autodownload.js
+++ plugins/_autodownload.js
@@ -12,7 +12,6 @@ handler.before = async function (m, { isOwner }) {
   if (chat.isBanned || user.banned || !chat.download || m.isBaileys) return
   
   if (/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)/i.test(m.text)) {
-    await m.reply('Proses')
     hx.igdl(m.text.match(/https?:\/\/(www\.)?instagram\.com\/(p|reel|tv)\/.*/i)[0].split(/\n| /i)[0]).then(async (r) => {
       for (let i = 0; i < r.medias.length; i++) {
       conn.sendFile(m.chat, r.medias[i].url, '', `${conn.user.name} Instagram downloader`, m)
@@ -21,34 +20,32 @@ handler.before = async function (m, { isOwner }) {
       }
    
    if (ytIdRegex.test(m.text) || ytIdRegex.test(m.selectedButtonId)) {
-     await m.reply('Proses')
         let yt = false
         let usedServer = servers[0]
         for (let i in servers) {
             let server = servers[i]
             try {
-                yt = await yta(vid.url, server)
-                yt2 = await ytv(vid.url, server)
+                yt = await yta(m.text.match(ytIdRegex.*/i)[0].split(/\n| /i)[0]), server)
+                yt2 = await ytv(m.text.match(ytIdRegex.*/i)[0].split(/\n| /i)[0]), server)
                 usedServer = server
                 break
             } catch (e) {
                // m.reply(`Server ${server} error!${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
             }
         }
-        if (yt === false) return m.reply(eror)
-        if (yt2 === false) return m.reply(eror)
+        // if (yt === false) return m.reply(eror)
+        // if (yt2 === false) return m.reply(eror)
         let { thumb, title, filesizeF } = yt
         await this.send2ButtonLoc(m.chat, thumb, `
 *Judul:* ${title}
 *Size Audio:* ${filesizeF}
 *Size Video:* ${yt2.filesizeF}
 *Server:* ${usedServer}
-`.trim(), '© Robby Catur', 'MP3', `.yta ${vid.url}`, 'MP4', `.ytv ${vid.url}`)
+`.trim(), '© Robby Catur', 'MP3', `.yta ${m.text.match(ytIdRegex.*/i)[0].split(/\n| /i)[0])}`, 'MP4', `.ytv ${m.text.match(ytIdRegex.*/i)[0].split(/\n| /i)[0])}`)
     }
     
     if (/https?:\/\/(fb\.watch|(www\.|web\.|m\.)?facebook\.com)/i.test(m.text)) {
-      await m.reply('Proses')
-      let res = await fetch('https://masgimenz.my.id/facebook/?url=' + m.text.match(/https?:\/\/(fb\.watch|(www\.|web\.|m\.)?facebook\.com)\/.*/i)[0].split(/\n| /i)[0])
+      let res = await fetch('https://masgimenz.my.id/facebook/?url=' + m.text.match(/https?:\/\/(fb\.watch|(www\.|web\.|m\.)?facebook\.com)\/.*/i)[0].split(/\n| /i)[0]
       let json = await res.json()
       let url = json.videoUrl
       await this.sendFile(m.chat, url, 'fb.mp4', `${conn.user.name} Facebook downloader`, m)
