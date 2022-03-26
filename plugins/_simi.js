489e019a256f724b899526e2defbfd083ba60326
RobbyCatur <79045499+RobbyCatur@users.noreply.github.com>
2022-03-26 20:53:57 +0700

    Update _simi.js

--- plugins/_simi.js
+++ plugins/_simi.js
@@ -10,7 +10,7 @@ handler.before = async (m) => {
         let res = await fetch(`https://api.simsimi.net/v2/?text=` + m.text + `&lc=id`)
         //if (!res.ok) return m.reply(`${res.status} ${res.statusText}`)
         let json = await res.json()
-        if (!json.status) return m.reply(require('util').format(json))
+        //if (!json.status) return m.reply(require('util').format(json))
         m.reply(json.success)
         return !0
     }
